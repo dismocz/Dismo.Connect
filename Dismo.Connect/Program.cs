2df7895b4f5998c9ecbde92a07b195287c8bfe63
@david.janousek <david.janousek@idioma.com>
2021-02-10 17:56:29 +0100

    bluetooth test

--- Dismo.Connect/Program.cs
+++ Dismo.Connect/Program.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Blazm.Bluetooth;
+using MudBlazor.Services;
 
 namespace Dismo.Connect
 {
@@ -18,6 +20,10 @@ namespace Dismo.Connect
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMudServices();
+
+            builder.Services.AddBlazmBluetooth();
 
             await builder.Build().RunAsync();
         }
